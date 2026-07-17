ac343bd99ce779db986d4367342a484c46e32fe2
mkriner-vs <mike@verasight.io>
2026-07-17 11:35:13 -0400

    update logo sizing

--- image_generation_app.py
+++ image_generation_app.py
@@ -21,7 +21,7 @@ st.set_page_config(page_title="Custom Image Generator", layout="wide")
 st.title("🎨 Custom Image Generator")
 st.write("Generate custom images by combining a template with overlays and text.")
 
-logo = Image.open("static/logo_white.png").resize((150, 50))
+logo = Image.open("static/logo_white.png").resize((250, 150))
 st.sidebar.image(logo)
 
 
